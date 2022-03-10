130371b4b1627097b2c37927899e6c989d09bc82
Yuldoshev <i.yuldoshev93@gmail.com>
2022-03-10 17:17:23 +0500

    Cat vs Dog

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:tflite/tflite.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,6 +13,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final picker = ImagePicker();
   File? _image;
+  bool _loading = false;
+  List? _output;
   pickImage() async {
     var image = await picker.pickImage(source: ImageSource.camera);
 
@@ -22,6 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _image = File(image.path);
     });
+    classifyImage(_image!);
   }
 
   pickGalleryImage() async {
@@ -34,14 +38,51 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _image = File(image.path);
     });
+    classifyImage(_image!);
+  }
+
+  @override
+  void initState() {
+    _loading = true;
+    loadModel().then((value) {
+      //
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _loading = false;
+      _output = output;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF101010),
+      backgroundColor: const Color(0xFF101010),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -55,10 +96,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 fontSize: 15,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 6,
             ),
-            Text(
+            const Text(
               "Detect Cats and Dogs",
               style: TextStyle(
                 color: Color(0xFFE99600),
@@ -66,23 +107,46 @@ class _HomeScreenState extends State<HomeScreen> {
                 fontWeight: FontWeight.w500,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 50,
             ),
             Center(
-              child: Container(
-                width: 300,
-                child: Column(
-                  children: [
-                    Image.asset("assets/cat.png"),
-                    SizedBox(
-                      height: 50,
+              child: _loading
+                  ? SizedBox(
+                      width: 300,
+                      child: Column(
+                        children: [
+                          Image.asset("assets/cat.png"),
+                          const SizedBox(
+                            height: 50,
+                          ),
+                        ],
+                      ),
+                    )
+                  : Container(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 250.0,
+                            child: Image.file(_image!),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          _output != null
+                              ? Text(
+                                  "${_output![0]['label']}",
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                )
+                              : Container(),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
             ),
-            Container(
+            SizedBox(
               width: MediaQuery.of(context).size.width,
               child: Column(
                 children: [
@@ -91,23 +155,23 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Container(
                       width: MediaQuery.of(context).size.width - 260,
                       alignment: Alignment.center,
-                      padding: EdgeInsets.symmetric(
+                      padding: const EdgeInsets.symmetric(
                         horizontal: 24,
                         vertical: 17,
                       ),
                       decoration: BoxDecoration(
-                        color: Color(
+                        color: const Color(
                           0xFFE99600,
                         ),
                         borderRadius: BorderRadius.circular(6),
                       ),
-                      child: Text(
+                      child: const Text(
                         "Take Photo",
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 50,
                   ),
                   GestureDetector(
@@ -115,17 +179,17 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Container(
                       width: MediaQuery.of(context).size.width - 260,
                       alignment: Alignment.center,
-                      padding: EdgeInsets.symmetric(
+                      padding: const EdgeInsets.symmetric(
                         horizontal: 24,
                         vertical: 17,
                       ),
                       decoration: BoxDecoration(
-                        color: Color(
+                        color: const Color(
                           0xFFE99600,
                         ),
                         borderRadius: BorderRadius.circular(6),
                       ),
-                      child: Text(
+                      child: const Text(
                         "Camera Roll",
                         style: TextStyle(color: Colors.white),
                       ),
